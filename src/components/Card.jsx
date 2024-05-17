d2511fc884b13dc8a88ce1f9eb777876074de927
if3ll <sunnwydays@gmail.com>
2024-05-17 18:57:22 -0400

    hi

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
         <div className="card">
         <img className="card-image" src={mypic} alt="profile picture" width="200"></img>
         <h2 className="card-title">Sunny</h2>
-        <p className="card-text">Computer engineering at UofT</p>
+        <p className="card-text">engineering</p>
     </div>
     )
 }
